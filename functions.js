a11d34f5f43ea7256428d51ba56f0d1dff67c160
Gabriel Hess <heg2@bfh.ch>
2020-09-16 13:17:28 +0200

    get article url from server

--- functions.js
+++ functions.js
@@ -1,4 +1,12 @@
 const request = require('request');
+const cheerio = require('cheerio');
+
+const cleanString = (string) => {
+    if (string) {
+        return string.replace(/\n/g, '').trim();
+    }
+    return undefined;
+}
 
 module.exports = {
     /**
@@ -6,8 +14,9 @@ module.exports = {
     * @param _url         the url where we can log in
     * @param _credentials an object with the users credentials as {name: string, pass: string}
     *                     where name is the login name and pass is the login password
-    * @return             an array of cookies as {cookie: string, expires: date}, where cookie
-    *                     is the cookie key/value pair and expires is the cookies expiration date.
+    * @return             a Promise with an array of cookies as {cookie: string, expires: date},
+    *                     where cookie is the cookie key/value pair and expires is the cookies
+    *                     expiration date.
     */
     getLoginCookie: function(_url, _credentials){
         return new Promise((resolve,reject) => {
@@ -36,15 +45,69 @@ module.exports = {
         });
     },
 
-    getArticleUrl: function(_url) {
+    /**
+     * Fetches the urls for every article
+     * @param _baseurl the base url, like https://woz.ch
+     * @param _subpath the path to the article overview, relative to the base url
+     * @return         a Promise with an object representing the issue of the newspaper
+     *                 with following structure:
+     *                 {
+     *                     issue: string,
+     *                     imageUrl: string,
+     *                     sections: [
+     *                         {
+     *                             title: string,
+     *                             articles: [
+     *                                 {
+     *                                     url: string,
+     *                                     title: string,
+     *                                     subtitle: string
+     *                                 }
+     *                             ]
+     *                         }
+     *                     ]
+     *                 }
+     */
+    getArticleUrls: function(_baseUrl, _subpath) {
         return new Promise((resolve,reject) => {
             request({
                 'method': 'GET',
-                'url': _url,
+                'url': _baseUrl + '/' + _subpath,
                 'headers': {}
-            }, (error, response) => {
+            }, (error, response, html) => {
                 if (error) reject(error);
-                console.log(response.body);
+                if (response && response.statusCode === 200) {
+                    const $ = cheerio.load(html);
+                    const woz = {
+                        issue: $('nav[class=wozissueindex-pager] > h2').html(),
+                        imageUrl: $('div[class=content] > picture > img').attr('src').split('?')[0],
+                        sections: []
+                    }
+                    $('section[class=wozissueindex-section]').each((i, _section) => {
+                        const section = {
+                            title: cleanString($(_section).children('h3').text()),
+                            articles: []
+                        }
+                        $(_section).children('ul').children('li').each((i, listItem) => {
+                            const link = $(listItem).children('article').children('h1').children('a');
+                            const article = {
+                                url: $(link).attr('href') ? cleanString(_baseUrl + $(link).attr('href')) : undefined,
+                                title: cleanString($(link).text()),
+                                subtitle: cleanString($(listItem).children('article').children('h2').text())
+                            }
+                            if (article.url) {
+                                section.articles.push(article);
+                            }
+                        });
+                        if (section.articles.length > 0) {
+                            woz.sections.push(section);
+                        }
+                    });
+                    resolve(woz);
+                } else {
+                    console.log('Fetching Article URLs, HTTP Status: ' + response.statusCode);
+                    reject(response.statusCode);
+                }
             });
         });
     }
